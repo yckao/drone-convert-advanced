5b0b94a2b70a95bd1752cd2a466375cc5bd3a526
yckao <yckaokarl@gmail.com>
2020-05-31 09:40:39 +0900

    build: disable cgo

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,9 @@
 set -e
 set -x
 
+# disable cgo
+export CGO_ENABLED=0
+
 # linux
 GOOS=linux GOARCH=amd64 go build -o ./release/linux/amd64/drone-convert-advanced ./cmd/drone-convert-advanced
 GOOS=linux GOARCH=arm64 go build -o ./release/linux/arm64/drone-convert-advanced ./cmd/drone-convert-advanced
